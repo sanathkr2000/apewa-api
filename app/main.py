a09f068bbb516cdddb0900a265b8ed2fecce340e
sanathkr2000 <sanathkumark@newmeksolutions.com>
2025-08-01 12:29:27 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI, HTTPException
 from app.db.database import database
 from app.logging_conf import configure_logging
 from fastapi.exception_handlers import http_exception_handler
-from app.routers.register import register_router
+
+from app.routers import userlogin
+from app.routers.admin import admin_router
+from app.routers.register import  register_router
 from app.routers.user_routes import user_router
 from app.routers.userlogin import user_login_router
 
@@ -35,5 +38,5 @@ app.include_router(user_router)
 
 @app.exception_handler(HTTPException)
 async def http_exception_handle_logging(request, exc):
-    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    logger.error(f"HTTPException: {exc.status_code}{exc.detail}")
     return await http_exception_handler(request, exc)
